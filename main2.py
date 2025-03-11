d428a1fb9d7394d617f991cf7f691302895650f8
veskok <veskok@ttu.ee>
2025-03-11 15:19:25 +0100

    updated xiampi

--- main2.py
+++ main2.py
@@ -34,6 +34,7 @@ cv.namedWindow('img')
 cv.createTrackbar('param1', 'img', 50, 100, nothing)
 cv.createTrackbar('param2', 'img', 30, 100, nothing)
 cv.createTrackbar('radius', 'img', 1, 100, nothing)
+cv.createTrackbar('radius_max', 'img', 100, 100, nothing)
 
 switch = '0 : OFF \n1 : ON'
 cv.createTrackbar(switch, 'img', 1, 1, nothing)
@@ -45,7 +46,7 @@ while True:
     frame = img.get_image_data_numpy()
 
     # Resize for display
-    frame = cv.resize(frame, (240, 240))
+    frame = cv.resize(frame, (640, 480))
 
     # Convert to grayscale
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -55,6 +56,7 @@ while True:
     p1 = cv.getTrackbarPos('param1', 'img')
     p2 = cv.getTrackbarPos('param2', 'img')
     r = cv.getTrackbarPos('radius', 'img')
+    rmax = cv.getTrackbarPos('radius_max', 'img')
     s = cv.getTrackbarPos(switch, 'img')
 
     # Create a copy of the frame for drawing circles
@@ -62,13 +64,18 @@ while True:
 
     if s == 1:
         # Detect circles using trackbar parameters
-        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 20, param1=p1, param2=p2, minRadius=r, maxRadius=100)
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 20, param1=p1, param2=p2, minRadius=r, maxRadius=rmax)
 
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
-                cv.circle(output, (i[0], i[1]), i[2], (0, 255, 0), 2)  # Draw outer circle
-                cv.circle(output, (i[0], i[1]), 2, (0, 0, 255), 3)  # Draw center
+                x, y, radius = i
+                cv.circle(output, (x, y), radius, (0, 255, 0), 2)  # Draw outer circle
+                cv.circle(output, (x, y), 2, (0, 0, 255), 3)  # Draw center
+
+                # Display the radius as text near the detected circle
+                cv.putText(output, f'r: {radius}', (x - 20, y - 10),
+                           cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     # Show the processed frame
     cv.imshow('img', output)
